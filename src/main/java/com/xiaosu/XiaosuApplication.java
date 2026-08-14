ce57b9b6fd66f6b4025bc8859d9c8e43c9c2d7f0
pirate-hcf <3370063660@qq.com>
2026-08-14 22:27:19 +0800

    chore: initialize spring boot project

---
+++ src/main/java/com/xiaosu/XiaosuApplication.java
@@ -0,0 +1,12 @@
+package com.xiaosu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaosuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaosuApplication.class, args);
+    }
+}
